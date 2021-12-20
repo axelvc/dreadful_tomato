9365e0d8f605cbf8b61cfc4a76209fa1e06bd2fe
Axel Vasquez <a.axelvc@gmail.com>
2021-12-20 06:32:17 -0600

    fix: :bug: fix ref error in NavLink component

--- src/shared/Header/NavLink.jsx
+++ src/shared/Header/NavLink.jsx
@@ -1,14 +1,13 @@
+import { forwardRef } from 'react'
 import { NavLink as RNavLink } from 'react-router-dom'
 import { HeaderMenuItem } from 'carbon-components-react'
 
 import s from './Header.module.scss'
 
-const NavLink = ({ href, className, children }) => {
-  return (
-    <HeaderMenuItem element={RNavLink} to={href} className={s.navLink}>
-      <span className={`${s.navLink} ${className}`}>{children}</span>
-    </HeaderMenuItem>
-  )
-}
+const NavLink = forwardRef(({ href, className, children }, ref) => (
+  <HeaderMenuItem element={RNavLink} to={href} className={s.navLink}>
+    <span className={`${s.navLink} ${className}`}>{children}</span>
+  </HeaderMenuItem>
+))
 
 export default NavLink
--- src/shared/Header/Navbar.jsx
+++ src/shared/Header/Navbar.jsx
@@ -1,16 +1,14 @@
 import { HeaderNavigation } from 'carbon-components-react'
 import NavLink from './NavLink'
 
-const Navbar = ({ title, routes }) => {
-  return (
-    <HeaderNavigation aria-label={title}>
-      {routes.map(({ href, label, className }) => (
-        <NavLink key={label} href={href} className={className}>
-          {label}
-        </NavLink>
-      ))}
-    </HeaderNavigation>
-  )
-}
+const Navbar = ({ title, routes }) => (
+  <HeaderNavigation aria-label={title}>
+    {routes.map(({ href, label, className }) => (
+      <NavLink key={label} href={href} className={className}>
+        {label}
+      </NavLink>
+    ))}
+  </HeaderNavigation>
+)
 
 export default Navbar
